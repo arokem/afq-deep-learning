0bb28b21d5f08be2374779b97a36d711ebf7bf89
Joanna Qiao <joanna@Joannas-MacBook-Pro.local>
2021-11-06 01:29:25 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository hosts some work-in-progress experiments applying deep learning to predict age using tractometry data.',
+    author='Joanna Qiao',
+    license='BSD-3',
+)
